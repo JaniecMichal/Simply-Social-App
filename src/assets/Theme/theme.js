bd33517dee1dd9651dc8677fc3bc699c66ceea50
Michal Janiec <michal.janiec95@gmail.com>
2021-04-16 23:41:45 +0200

    Created GlobalStyles and Theme

---
+++ src/assets/Theme/theme.js
@@ -0,0 +1,24 @@
+export const theme = {
+  fontSize: {
+    small: '12px',
+    normal: '16px',
+    large: '18px',
+    xlarge: '38px',
+  },
+  colors: {
+    mainBackground: '#C0C7D6',
+    mainText: '#000000',
+    text: '#fff',
+    yaleBlue: '#274C77',
+    frost: '#6096BA',
+    bbEyes: '#A3CEF1',
+    bGrey: '#8B8C89',
+    platinum: '#E1E1E0',
+  },
+  media: {
+    smallMobile: '340px',
+    mobile: '767.98px',
+    tablet: '991.98px',
+    desktop: '1199.98px',
+  },
+};
