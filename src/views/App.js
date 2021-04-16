4edf623a5585052b2946ee9ae8e6994dc7479d97
Michal Janiec <michal.janiec95@gmail.com>
2021-04-16 23:42:54 +0200

    Changed rendered elements in main component app. Created MainWrapper

--- src/views/App.js
+++ src/views/App.js
@@ -1,24 +1,12 @@
-import logo from 'assets/logo.svg';
-import './App.css';
+import React from 'react';
+import { MainWrapper } from './styled';
+import UserPanel from 'components/molecules/UserPanel';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MainWrapper>
+      <UserPanel />
+    </MainWrapper>
   );
 }
 
---
+++ src/views/styled.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const MainWrapper = styled.main`
+  width: 100%;
+  max-width: 1680px;
+  margin: 10px auto 0px auto;
+`;
