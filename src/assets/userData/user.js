7417f282d587d7a33410943889544fe1b165febd
Michal Janiec <michal.janiec95@gmail.com>
2021-04-16 23:41:59 +0200

    Added data for logged user

---
+++ src/assets/userData/user.js
@@ -0,0 +1,8 @@
+export const user = {
+  name: 'Michał Janiec',
+  position: 'Junior Frontend Developer',
+  description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque iaculis,
+  turpis a auctor aliquet, ante ipsum accumsan mauris, in dictum velit nibh
+  in dolor. In hac habitasse platea dictumst. Donec et nisl faucibus tellus
+  condimentum semper eget sed dui.`,
+};
