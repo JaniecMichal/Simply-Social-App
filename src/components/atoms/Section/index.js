df6c448c9eba68158d61d9ad716e62dfc5d65102
Michal Janiec <michal.janiec95@gmail.com>
2021-04-17 02:03:43 +0200

    Removed file, created two single components

---
+++ src/components/atoms/Section/index.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Section = styled.section`
+  max-width: 100%;
+  margin-top: 50px;
+  padding: 30px;
+  background-color: ${({ theme }) => theme.colors.platinum};
+`;
--- src/components/atoms/Title/index.js
+++ src/components/atoms/Title/index.js
@@ -1,12 +1,5 @@
 import styled from 'styled-components';
 
-export const AddingPost = styled.section`
-  max-width: 100%;
-  margin-top: 50px;
-  padding: 30px;
-  background-color: ${({ theme }) => theme.colors.platinum};
-`;
-
 export const Title = styled.h3`
   font-size: ${({ theme }) => theme.fontSize.large};
   font-weight: 700;
