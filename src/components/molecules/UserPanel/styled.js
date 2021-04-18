b0ea268ebe181117128f9c72b5d1779231784db8
Michal Janiec <michal.janiec95@gmail.com>
2021-04-18 23:07:09 +0200

    Changed html tag

--- src/components/molecules/UserPanel/styled.js
+++ src/components/molecules/UserPanel/styled.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const StyledHeader = styled.header`
+export const StyledHeader = styled.div`
   padding: 10px 10px;
   position: absolute;
   right: 0;
